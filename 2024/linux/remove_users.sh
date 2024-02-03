93119b2156c6b30e1dbba917c375ee0731a11efa
Jack Brower <ejbrower4@gmail.com>
2024-02-03 12:59:06 -0500

    Update remove_users.sh

--- 2024/linux/remove_users.sh
+++ 2024/linux/remove_users.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-while read line
+awk -F: '($3>=1000)&&($1!="nobody"){print $1}' /etc/passwd > all_users.txt
+
+grep -vxFf ./good_users.txt ./all_users.txt | while read user
 do
-	touch all_users.txt
-	$(awk -F: '($3>=1000)&&($1!="nobody"){print $1}' /etc/passwd) > all_users.txt
-	$(comm -23 all_users.txt -) > remove_users.txt
-	#for user in $(cat remove_users.txt)
-	#do
-	#	$(userdel -r $user)
-	#	echo "User $user removed"
-	#done
-done < "${1:-/dev/stdin}"
+	#$(userdel -r $user)
+ 	echo "User $user removed"
+done
