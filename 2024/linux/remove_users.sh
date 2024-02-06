b1bf26370e95ceba4796bc8bac9f93adae573e77
Jack Brower <ejbrower4@gmail.com>
2024-02-06 11:33:15 -0500

    Update remove_users.sh

--- 2024/linux/remove_users.sh
+++ 2024/linux/remove_users.sh
@@ -5,5 +5,5 @@ awk -F: '($3>=1000)&&($1!="nobody"){print $1}' /etc/passwd > all_users.txt
 grep -vxFf ./good_users.txt ./all_users.txt | while read user
 do
 	#$(userdel -r $user)
- 	echo "User $user removed"
+ 	echo "$user is a bad user! Edit script and RERUN!"
 done
